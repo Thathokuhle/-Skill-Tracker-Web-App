ba20b58a2a08e3924c983aaca8edfa65bb0d3864
Thathokuhle <simthandathathokuhle@gmail.com>
2025-06-04 14:48:04 +0200

    Repository

---
+++ Repository/RepositoryInterfaces/iAccountRepository.cs
@@ -0,0 +1,11 @@
+﻿using DataLogic.Account;
+
+namespace Repository.RepositoryInterfaces
+{
+    public interface iAccountRepository
+    {
+        Task<GetUserByEmail?> GetUserByEmail(string email, CancellationToken token = default);
+        Task CreateAccount(CreateAccount account);
+        Task<UserRole?> GetUserRole(string email, CancellationToken token = default);
+    }
+}
