d0464ed2162c08bb0d2c0599b28464e5e6c7c2f1
Thathokuhle <simthandathathokuhle@gmail.com>
2025-06-04 14:30:36 +0200

    Branch fixed up after 9hrs

---
+++ SkillTrackerApp/Pages/Data/HelpAndSupport.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SkillTrackerApp.Pages.Data
+{
+    public class HelpAndSupportModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
